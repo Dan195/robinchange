92392bee886c011b6d453c37eed3c861401d6e77
Dan195 <dan.lg.1023@gmail.com>
2020-07-02 08:38:07 -0500

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,36 @@
+#URL
+BASE_URL = "http://robintrack.net"
+POPULARITY_CHANGES = "/popularity_changes"
+
+#COLUMN NAMES
+CUR_DAY = "cur_day"
+DATE = "date"
+PREV_DAY = "prev_day"
+PRIMARY_SYMBOL = "primary_symbol"
+SYMBOL = "symbol"
+TWEET_TEXT = "time_text"
+TIMESTAMP = "timestamp"
+
+#SELECTORS
+CLASS_TOGGLE_RELATIVE = "bp3-control-indicator"
+CLASS_USER_CHANGE = "ReactVirtualized__Table__rowColumn"
+SEL_SELECT = "select"
+SEL_MIN_POP = 'input[type="text"]'
+
+#LOOKBACK_PERIODS
+ONE_HOUR = "1"
+FOUR_HOURS = "4"
+ONE_DAY = "24"
+THREE_DAYS = "72"
+ONE_WEEK = "172"
+THIRTY_DAYS = "720"
+
+#CHANGE TYPE
+CHANGE_INC = "increases"
+CHANGE_DEC = "decreases"
+
+#POSITIONS
+SYMBOL_DIFF = 1
+CHANGE_DIFF = 2
+USERS_PREV_DAY_DIFF = 3
+USERS_CUR_DAY_DIFF = 4
